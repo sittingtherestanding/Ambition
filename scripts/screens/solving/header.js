9d72320372e253243bdb1cde073acd4029948c57
Jesse Millar <hellojessemillar@gmail.com>
2014-07-18 17:26:09 -0600

    Added page switching.

--- scripts/screens/solving/header.js
+++ scripts/screens/solving/header.js
@@ -7,8 +7,22 @@ var Header = function()
 	var typewriter = new Typewriter()
 		typewriter.setColor(secondaryColor)
 
+	var storeButton = new Entity()
+		storeButton.setPosition(0, 0)
+				   .setSize(l.room.width, sizeTitle * 3)
+
+	this.watch = function()
+	{
+		if (mouse.checkLeftClicked(storeButton))
+		{
+			currentScreen = 'store'
+		}
+	}
+
 	this.draw = function()
 	{
+		this.watch()
+
 		typewriter.setSize(sizeTitle).setPosition(padding, padding).setAlignment('left').write('$ ' + money)
 		typewriter.setSize(sizeTitle).setPosition(l.room.width / 2, padding).setAlignment('center').write(jobs[promotion])
 		typewriter.setSize(sizeTitle).setPosition(l.room.width - padding, padding).setAlignment('right').write('SSP ' + social)
