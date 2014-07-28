ba488483932261566a333d621917865ebc9f3d31
Jesse Millar <hellojessemillar@gmail.com>
2014-07-28 13:57:51 -0600

    Revert 339dc5f..23b8b0b
    This rolls back to commit 339dc5f954a1a4d83713f2c8a1b068579a37c544.

--- scripts/screens/spend/inventory.js
+++ scripts/screens/spend/inventory.js
@@ -1,28 +1,63 @@
 var Inventory = function()
 {
-	var buttoner = new Buttoner()
+	var size = 20
+	var padding = 15
 
-	this.purchase = function(index)
+	var header = new Header()
+
+	var top = header.height + padding * 3
+	var itemSize = padding * 5
+
+	var typewriter = new Typewriter()
+		typewriter.setSize(size).setColor(black).setFont('bebas_neueregular').setBaseline('top')
+
+	var canClick = false
+
+	this.drawButton = function(buttonID)
 	{
-		console.log(index)
+		typewriter.setAlignment('left').setColor(aqua).setPosition(padding, top + itemSize * (buttonID)).write(inventoryItems[buttonID].name)
+		typewriter.setAlignment('left').setColor(black).setPosition(padding, top + itemSize * (buttonID) + padding * 1.5).write('purchased 0 times')
 
-		if (money >= inventoryItems[index].price && !displayMenu)
+		typewriter.setAlignment('right').setColor(lime).setPosition(l.room.width - padding, top + itemSize * (buttonID)).write('$' + inventoryItems[buttonID].price)
+		typewriter.setAlignment('right').setColor(gray).setPosition(l.room.width - padding, top + itemSize * (buttonID) + padding * 1.5).write(inventoryItems[buttonID].points + ' SSP')
+	}
+
+	this.watch = function()
+	{
+		if (mouse.leftClick && canClick)
 		{
-			money -= inventoryItems[index].price
-			social += inventoryItems[index].points
+			canClick = false
 
-			inventoryItems[index].purchased = true
+			var index = Math.floor((mouse.y - top) / itemSize)
+
+			if (index >= 0) // Make it so we can't click on the header to buy things
+			{
+				this.purchase(index)
+			}
 		}
 	}
+
+		this.purchase = function(index)
+		{
+			if (money >= inventoryItems[index].price && !displayMenu)
+			{
+				money -= inventoryItems[index].price
+				social += inventoryItems[index].points
+
+				inventoryItems[index].purchased = true
+			}
+		}
 
 	this.draw = function()
 	{
 		game.color = maroon
 
-		if (buttoner.watch)
+		if (!mouse.leftClick)
 		{
-			this.purchase(buttoner.watch)
+			canClick = true
 		}
+
+		this.watch()
 
 		var i = inventoryItems.length
 		while (i--)
